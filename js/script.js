ec9b6c0e33049618e58c7cdb20fc5b6a85fa5b09
Francesco <francescococlite@gmail.com>
2021-05-27 15:11:30 +0200

    sconto under 18

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole: il prezzo del biglietto è definito in base ai km (0.21 € al km) va applicato uno sconto del 20% per i minorenni va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 
+var eta = parseInt(prompt('Qunati anni hai?'))
+console.log(eta);
+
+
 var kmDafare =  parseInt(prompt('Quanti chilometri devono essere fatti?'));
 console.log(kmDafare);
 
 var risultato = kmDafare * 0.21;
 console.log(risultato);
 
-
-if
-
+if (eta <= 17){
+    risultato = (risultato - (risultato * 0.2)).toFixed(2);
+    console.log(risultato);
+}
+else (eta >= 65)
