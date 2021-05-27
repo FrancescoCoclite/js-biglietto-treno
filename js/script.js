35026f37901e6647dcd45a95be6caf72f41ccb4e
Francesco <francescococlite@gmail.com>
2021-05-27 14:50:31 +0200

    prezzo per km

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-//il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-//Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole: il prezzo del biglietto è definito in base ai km (0.21 € al km) va applicato uno sconto del 20% per i minorenni va applicato uno sconto del 40% per gli over 65.
-//L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). 
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole: il prezzo del biglietto è definito in base ai km (0.21 € al km) va applicato uno sconto del 20% per i minorenni va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+var kmDafare =  parseInt(prompt('Quanti chilometri devono essere fatti?'));
+console.log(kmDafare);
+
+var risultato = kmDafare * 0.21;
+console.log(risultato);
+
+
+if
+
